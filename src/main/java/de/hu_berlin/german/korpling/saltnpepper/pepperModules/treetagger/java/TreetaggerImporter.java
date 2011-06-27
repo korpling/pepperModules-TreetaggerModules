e478e199a08675f91a577b3fa1e9640b6c4c9541
hildebax <hildebax@mycompany.com>
2011-06-27 12:29:13 +0000

    Renaming TreetaggerImporterJava and TreetaggerExporterJava to TreetaggerImporter and TreetaggerExporter
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/PepperModules/TreetaggerModules/trunk@1302 47085195-c50e-4317-8f43-7cb808db0f00

---
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/treetagger/java/TreetaggerImporter.java
@@ -0,0 +1,237 @@
+/**
+ * Copyright 2009 Humboldt University of Berlin, INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+package de.hu_berlin.german.korpling.saltnpepper.pepperModules.treetagger.java;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.log.LogService;
+
+import de.hu_berlin.german.korpling.saltnpepper.misc.treetagger.Document;
+import de.hu_berlin.german.korpling.saltnpepper.misc.treetagger.resources.TabResource;
+import de.hu_berlin.german.korpling.saltnpepper.misc.treetagger.resources.TabResourceFactory;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperExceptions.PepperModuleException;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.FormatDefinition;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.PepperImporter;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.PepperInterfaceFactory;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.RETURNING_MODE;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.impl.PepperImporterImpl;
+import de.hu_berlin.german.korpling.saltnpepper.pepperModules.treetagger.exceptions.TreetaggerImporterException;
+import de.hu_berlin.german.korpling.saltnpepper.pepperModules.treetagger.mapper.Treetagger2SaltMapper;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpusGraph;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SDocument;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SElementId;
+
+@Component(name="TreetaggerImporterComponent", factory="PepperImporterComponentFactory")
+@Service(value=PepperImporter.class)
+
+//TODO: multiple documents in one file
+
+public class TreetaggerImporter extends PepperImporterImpl implements PepperImporter
+{
+
+	//---------------------------------------------------------------------------------------
+	private void log(int logLevel, String logText) {
+		if (this.getLogService()!=null) {
+			this.getLogService().log(logLevel, "<TreetaggerImporter>: " + logText);
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private void logError  (String logText) { this.log(LogService.LOG_ERROR,   logText); }
+	private void logWarning(String logText) { this.log(LogService.LOG_WARNING, logText); }
+	@SuppressWarnings("unused")
+	private void logInfo   (String logText) { this.log(LogService.LOG_INFO,    logText); }
+	private void logDebug  (String logText) { this.log(LogService.LOG_DEBUG,   logText); }
+
+	//---------------------------------------------------------------------------------------
+	private Properties properties = null;
+	
+	public Properties getProperties() {
+		return this.properties;
+	}
+	
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+	//---------------------------------------------------------------------------------------
+	
+	public TreetaggerImporter()
+	{
+		super();
+		this.name= "TreetaggerImporter";
+		//for testing the symbolic name has to be set without osgi
+		if (	(this.getSymbolicName()==  null) ||
+				(this.getSymbolicName().equalsIgnoreCase("")))
+			this.setSymbolicName("de.hu_berlin.german.korpling.saltnpepper.pepperModules.TreetaggerModules");
+		this.init();
+		logDebug(this.getName()+" is created...");
+	}
+
+	protected void init()
+	{
+		this.supportedFormats= new BasicEList<FormatDefinition>();
+		FormatDefinition formatDef= PepperInterfaceFactory.eINSTANCE.createFormatDefinition();
+		formatDef.setFormatName("treetagger");
+		formatDef.setFormatVersion("1.0");
+		this.supportedFormats.add(formatDef);
+	}
+	
+	private EList<FormatDefinition> supportedFormats= null;
+	
+	@Override
+	public EList<FormatDefinition> getSupportedFormats() 
+	{
+		return(this.supportedFormats);
+	}
+
+	/**
+	 * Stores relation between documents and their resource 
+	 */
+	private Map<SElementId, URI> documentResourceTable= null;
+	
+	@Override
+	public void importCorpusStructure(SCorpusGraph corpusGraph)
+			throws PepperModuleException
+	{
+		this.setSCorpusGraph(corpusGraph);
+		if (this.getSCorpusGraph()== null)
+			throw new PepperModuleException(this.name+": Cannot start with importing corpus, because salt project isn�t set.");
+		
+		if (this.getCorpusDefinition()== null)
+			throw new PepperModuleException(this.name+": Cannot start with importing corpus, because no corpus definition to import is given.");
+		if (this.getCorpusDefinition().getCorpusPath()== null)
+			throw new PepperModuleException(this.name+": Cannot start with importing corpus, because the path of given corpus definition is null.");
+		if (this.getCorpusDefinition().getCorpusPath().isFile())
+		{
+			if (	(this.getCorpusDefinition().getCorpusPath().toFileString().endsWith("/")) || 
+					(this.getCorpusDefinition().getCorpusPath().toFileString().endsWith("\\")))
+			{//clean uri in corpus path (if it is a folder and ends with/, / has to be removed)
+				this.getCorpusDefinition().setCorpusPath(this.getCorpusDefinition().getCorpusPath().trimSegments(1));
+			}//clean uri in corpus path (if it is a folder and ends with/, / has to be removed)
+			
+			try {
+				this.documentResourceTable= this.createCorpusStructure(this.getCorpusDefinition().getCorpusPath(), null, null);
+			} catch (IOException e) {
+				throw new PepperModuleException(this.name+": Cannot start with importing corpus, because some exception occurs: ",e);
+			}
+		}	
+	}
+	
+	@Override
+	public void start(SElementId sElementId) throws PepperModuleException 
+	{
+		if (sElementId.getSIdentifiableElement()!= null)
+		{	
+			if (sElementId.getSIdentifiableElement() instanceof SDocument)
+			{	
+				this.returningMode= RETURNING_MODE.PUT;
+				URI uri= this.documentResourceTable.get(sElementId);
+				if (uri== null)
+					throw new TreetaggerImporterException("Cannot import document '"+sElementId+"', because no corresponding uri was found.");
+				
+				
+				if (this.getSpecialParams()!=null) {
+					String propertyFileName = this.getSpecialParams().toFileString();
+					try {
+						this.setProperties(new Properties());
+						this.getProperties().load(new FileInputStream(propertyFileName));
+					} catch (IOException e) {
+						logWarning(String.format("couldn´t load properties file '%s'. using default values.",propertyFileName));
+					}
+				}
+
+				Document tDocument = this.loadFromFile(uri);
+				if (tDocument==null) {
+					//TODO: take document out of the process
+				}
+				else {
+					Treetagger2SaltMapper mapper = new Treetagger2SaltMapper();
+					mapper.setProperties(this.getProperties());
+					mapper.setLogService(this.getLogService());
+					mapper.map(tDocument,this.getSCorpusGraph().getSDocument(sElementId));
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private Document loadFromFile(URI uri)
+	{
+		Document retVal= null;
+		if (uri!= null)
+		{
+			// create resource set and resource 
+			ResourceSet resourceSet = new ResourceSetImpl();
+	
+			// Register XML resource factory
+			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("treetagger",new XMIResourceFactoryImpl());
+			TabResourceFactory tabResourceFactory = new TabResourceFactory();
+			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("tab",tabResourceFactory);
+			resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("tt",tabResourceFactory);
+			Resource resource= null;
+			try {
+				//load resource 
+				resource = resourceSet.createResource(uri);
+
+				if (resource== null)
+					throw new TreetaggerImporterException("Cannot load The resource is null.");
+				
+				@SuppressWarnings("rawtypes")
+				//options map for resource.load
+				Map options = new HashMap();
+				//put logService for TabResource loading into options
+				options.put(TabResource.logServiceKey, this.getLogService());
+				//put properties for TabResource loading into options
+				options.put(TabResource.propertiesKey, this.getProperties());
+
+				resource.load(options);
+			} 
+			catch (IOException e) 
+			{	throw new TreetaggerImporterException("Cannot load resource '"+uri+"'.");	}
+			catch (NullPointerException e) 
+			{	throw new TreetaggerImporterException("Cannot load resource '"+uri+"'.");	}
+			if (resource.getContents().size()>0) {
+				retVal= (Document) resource.getContents().get(0);
+			}
+		}
+		return(retVal);
+	}
+	
+	protected void activate(ComponentContext componentContext) 
+	{
+		this.setSymbolicName(componentContext.getBundleContext().getBundle().getSymbolicName());
+	}
+
+	protected void deactivate(ComponentContext componentContext) {
+		logDebug(this.getName()+" is deactivated...");
+	}
+}
